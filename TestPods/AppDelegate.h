4f0be0ec5fcf9ad90b7a2e9491dc2ae1e88562a1
G.Z <zerodreaming@sina.com>
2016-04-21 20:59:12 +0800

    第一次pods修改测

---
+++ TestPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPods
+//
+//  Created by G.Z on 16/4/21.
+//  Copyright © 2016年 GZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPods/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  TestPods
+//
+//  Created by G.Z on 16/4/21.
+//  Copyright © 2016年 GZ. All rights reserved.
+//
+
+
+
+#import "ViewController.h"
+#import "afnet"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
